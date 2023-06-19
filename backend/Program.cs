68965c98baa3ab41ccc287d2da5c01aa472e0aa6
Raul Janari <rdjanari1707@gmail.com>
2023-06-19 12:33:31 +0200

    deleted

--- backend/Program.cs
+++ backend/Program.cs
@@ -13,12 +13,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
     app.UseAuthorization();
 
     app.MapControllers();
