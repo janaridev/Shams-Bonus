ddbad6ae8ffe3a4f408d04df5fa022a4a46b5f7d
Raul Janari <rdjanari1707@gmail.com>
2023-06-20 06:22:23 +0200

    CORS config

--- backend/Extensions/ServiceExtensions.cs
+++ backend/Extensions/ServiceExtensions.cs
@@ -7,6 +7,17 @@ namespace backend.Extensions;
 
 public static class ServiceExtensions
 {
+    public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+
     public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration,
         IWebHostEnvironment env)
     {
