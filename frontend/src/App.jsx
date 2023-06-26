fed7c2835218793254a39c4d4465b7738e96ac9c
Raul Janari <rdjanari1707@gmail.com>
2023-06-26 14:35:57 +0200

    React Redux base config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/loginPage";
 import HomePage from "./pages/homePage";
 import AdminPage from "./pages/adminPage";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/admin" element={<AdminPage />} />
+        <Route
+          path="/home"
+          element={isAuth ? <HomePage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/admin"
+          element={isAuth ? <AdminPage /> : <Navigate to="/" />}
+        />
       </Routes>
     </BrowserRouter>
   );
