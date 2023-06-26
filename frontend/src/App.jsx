10b501973d7ead27accd39a2ecc3d662cce15408
Raul Janari <rdjanari1707@gmail.com>
2023-06-26 20:18:19 +0200

    theme palettes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,34 @@ import LoginPage from "./pages/loginPage";
 import HomePage from "./pages/homePage";
 import AdminPage from "./pages/adminPage";
 import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route
-          path="/home"
-          element={isAuth ? <HomePage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/admin"
-          element={isAuth ? <AdminPage /> : <Navigate to="/" />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/admin"
+              element={isAuth ? <AdminPage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   );
 }
 
