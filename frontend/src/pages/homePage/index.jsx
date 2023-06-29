6baed6db7ded23dd4222f9a292b138876353b05e
Raul Janari <rdjanari1707@gmail.com>
2023-06-29 12:07:52 +0200

    Navbar

--- frontend/src/pages/homePage/index.jsx
+++ frontend/src/pages/homePage/index.jsx
@@ -1,17 +1,8 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import Navbar from "../navbar";
 
 const HomePage = () => {
-  const { firstName, lastName, userName, bonuses } = useSelector(
-    (state) => state.user
-  );
-  const showedInformation =
-    firstName || lastName === null ? userName : `${firstName} ${lastName}`;
-  return (
-    <>
-      {showedInformation} {bonuses}
-    </>
-  );
+  return <Navbar />;
 };
 
 export default HomePage;
